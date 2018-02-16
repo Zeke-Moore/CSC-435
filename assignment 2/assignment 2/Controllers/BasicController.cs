872b62f3665d7b56304899347c73d93203e42612
Daniel Moore "Zeke <danielezekielmoore@gmail.com>
2018-02-16 03:22:50 -0600

    part 1

---
+++ assignment 2/assignment 2/Controllers/BasicController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using assignment_2.Models;
+using System.Diagnostics;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace assignment_2.Controllers
+{
+    public class BasicController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WebForm()
+        {
+            ViewData["Message"] = "Please fill out the registration form below.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
